3abb2de939d201060a748efe2a3ecd52924c91e1
Mojtaba Charchian <mojishah70@gmail.com>
2024-02-05 19:54:21 +0330

    onlineTicket

---
+++ js/script.js
@@ -0,0 +1,42 @@
+﻿let countrySelectBox = document.querySelector(".countrySelect");
+let citySelectBox = document.querySelector(".citySelect");
+
+let countriesData = {
+    Iran : ['Tehran','Tabriz','Esfahan','Mashhad','Ahwaz'],
+    Turkey : ["Istanbul",'Ankara','Ezmir','Antaliya'],
+    USA : ['Los Angeles','San Diego','NewYork','Washington']
+}
+
+countrySelectBox.addEventListener('change',function(){
+    console.log('changed');
+    if(countrySelectBox.value=='Please Select'){
+        citySelectBox.innerHTML="";
+        citySelectBox.innerHTML += "<option>Select city</option>";
+    }else{
+        let mainCountryName=countrySelectBox.value;
+        let mainCountryCities=countriesData[mainCountryName];
+        console.log(mainCountryCities);
+        citySelectBox.innerHTML="";
+        mainCountryCities.forEach(function(x){
+            citySelectBox.innerHTML+="<option>"+x+"</option>";
+        });
+    }
+})
+
+
+// var users = [
+//   "Hossein",
+//   "Daeijoonjan",
+//   "Moji",
+//   "Mohammadjavd",
+//   "mammadreza",
+//   "alimelek",
+//   "Baba",
+// ];
+
+// users.forEach(function (name) {
+//   console.log(name);
+// });
+
+
+
